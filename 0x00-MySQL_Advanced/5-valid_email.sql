0b6428ea30287f523140928aa719e6cd6dbef1ac
loxiasblack <iliaschokairi@gmail.com>
2024-07-11 21:41:41 +0100

    push"

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create a trigger that reset valid_email
+-- only if email has been changed
+DELIMITER $$
+CREATE TRIGGER before_email_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;
+
