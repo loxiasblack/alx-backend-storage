52d2216ee8009798b1c7ade998055e80777aa3a7
loxiasblack <iliaschokairi@gmail.com>
2024-07-16 17:56:32 +0100

    update_many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,6 +4,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """update the topics of school collection"""
-    new_values = {"$set": {'topics': topics} }
+    new_values = {"$set": {'topics': topics}}
     filter = {'name' : name}
-    return mongo_collection.update_one(filter, new_values)
+    return mongo_collection.update_many(filter, new_values)
