1ac35335ad6879db1af0d0d320bc696548a451f1
loxiasblack <iliaschokairi@gmail.com>
2024-07-17 14:56:15 +0100

    push

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module to implement a simple caching system using Redis."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,26 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn:Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Retrieve data from the cache and optionally apply a conversion function."""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+    
+    def get_str(self, key:str) -> str:
+        """Get a string from the cache."""
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key:str) -> int:
+        """get int from the cahce."""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value 
+            
+        
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,9 +8,14 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+    print(type(value))
